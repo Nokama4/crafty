05f590eeecbc4c6ecf5da78eac85e372df27df17
N0kama <carine.daho@gmail.com>
2023-09-22 11:32:17 +0200

    Structure d'un test unitaire : connecter les étapes

--- src/tests/posting-message.spec.ts
+++ src/tests/posting-message.spec.ts
@@ -1,7 +1,7 @@
 describe("Feature: Posting a message", () => {
     describe("Rule: A message can contain up to 280 characters", () => {
         test("Alice can post a message on her timeline", () => {
-            givenNowIs(new Date("2020-08-01T09.00.00.000Z"));
+            givenNowIs(new Date("2020-08-01T09:00:00.000Z"));
 
             whenUserPostsMessage({
                 id: "message-id",
@@ -13,21 +13,37 @@ describe("Feature: Posting a message", () => {
                 id: "message-id",
                 text: "Hello world!",
                 author: "Alice",
-                publishedAt: new Date("2020-08-01T09.00.00.000Z")
+                publishedAt: new Date("2020-08-01T09:00:00.000Z")
             });
         });
     });
 });
 
-const givenNowIs = (now: Date) => {};
+let message: { id: string; text: string; author: string; publishedAt: Date; }
+let now: Date;
+
+const givenNowIs = (_now: Date) => {
+    now = _now;
+};
 const whenUserPostsMessage = (postMessageCommand: {
     id: string;
     text: string;
     author: string;
-}) => {};
+}) => {
+    message = {
+        id: postMessageCommand.id,
+        text: postMessageCommand.text,
+        author: postMessageCommand.author,
+        publishedAt: now
+    }
+};
 const thenpostedmessageShouldBe = (expectedMessage: {
     id: string;
     text: string;
     author: string;
     publishedAt: Date;
-}) => {}
+}) => {
+    expect(expectedMessage).toEqual(message);
+
+}
+
